ddfa298aeae99a8850e111e7a2420165bb24335f
SHAHINA AKTER <80826931+shahina0199@users.noreply.github.com>
2023-09-01 07:26:35 -0700

    Add files via upload

--- src/app/api/Set-cookie/route.js
+++ src/app/api/Set-cookie/route.js
@@ -1,30 +1,49 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { redirect } from 'next/navigation'
+  
  
+import { NextResponse } from 'next/server';
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    // Customize the preference value as needed (e.g., "theme: dark" or "language: en")
+    const preference = 'theme: dark';
+
+    // Set the preference cookie
+    res.setHeader('Set-Cookie', `user_preference=${preference}; Path=/`);
+
+    return NextResponse.json(
+      { status: true, message: 'User preference set successfully' },
+      { status: 201 }
+    );
+  }
+
+  return NextResponse.error('Invalid request method', { status: 405 });
+};
+
  
 
+ 
 
   
 
  
 
-export async function GET(request: NextRequest){
+// export async function GET(request: NextRequest){
 
-    const response =NextResponse, redirect (new URL('/src/app/blogpage', request.url), {status:302});
+//     const response =NextResponse, redirect (new URL('/src/app/blogpage', request.url), {status:302});
 
-    response.cookies.set({
+//     response.cookies.set({
 
-        Nane:'theme',
-        Value:'dark',
-        expires:new Date(2023, new Date ().getDate(), new Date (), getHours()+ 3,0,0),
-        path:'/',
-        Secure:true,
-        httpOnly:true,
-        SameSite:'strict'
+//         Name:'theme',
+//         Value:'dark',
+//         expires:new Date(2023, new Date ().getDate(), new Date (), getHours()+ 3,0,0),
+//         path:'/',
+//         Secure:true,
+//         httpOnly:true,
+//         SameSite:'strict'
 
 
-    })
+//     })
 
-    return response;
+//     return response;
 
-}
+// }
--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,50 @@
  
    
 
-  export default function Home(){
-    const handleSet=()=>{
+//   export default function Home(){
+//     const handleSet=()=>{
        
 
-      (async () =>{
-        await fetch('http://localhost:3000/api/Set-cookie')
-      }) ();
-    }
+//       (async () =>{
+//         await fetch('http://localhost:3000/api/Set-cookie')
+//       }) ();
+//     }
 
 
-  return(
+//   return(
               
+//     <div className="container">
+//       <p>Home Page</p>
+//       <button className="btn" onClick={handleSet}>
+//       Set-Cookie</button>
+
+//     </div>
+
+
+//   )
+
+// }
+  
+
+const Home = () => {
+  const handleSet = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/Set-cookie/route.js', {
+        method: 'POST',
+      });
+
+      if (response.ok) {
+        console.log('User preference set successfully');
+      } else {
+        console.error('Failed to set user preference');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+             
     <div className="container">
       <p>Home Page</p>
       <button className="btn" onClick={handleSet}>
@@ -22,8 +54,8 @@
     </div>
 
 
-  )
+  );
+};
 
-}
-  
+export default Home;
 
